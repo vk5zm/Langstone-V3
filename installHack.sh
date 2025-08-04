c2ad5f9a61102410688859013cfae7a6f10dd512
g4eml <colin@durbridge.me.uk>
2025-08-04 22:03:00 +0100

    Update installHack.sh

--- installHack.sh
+++ installHack.sh
@@ -15,7 +15,7 @@ echo "#################################"
 
 # Update the distribution
 sudo apt-get -y update
-sudo apt-get -y dist-upgrade
+#sudo apt-get -y dist-upgrade
 
 
 echo "#################################"
